fd0f4ab2be355c2af1eecff899a9fd9f9836c07a
Glenn Reyes <glenn@glennreyes.com>
2022-09-23 15:51:26 +0200

    fix: fix prettier plugin & run format

--- protocol/contracts/Lock.sol
+++ protocol/contracts/Lock.sol
@@ -1,19 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import 'hardhat/console.sol';
 
 contract Lock {
-    uint public unlockTime;
+    uint256 public unlockTime;
     address payable public owner;
 
-    event Withdrawal(uint amount, uint when);
+    event Withdrawal(uint256 amount, uint256 when);
 
-    constructor(uint _unlockTime) payable {
+    constructor(uint256 _unlockTime) payable {
         require(
             block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
+            'Unlock time should be in the future'
         );
 
         unlockTime = _unlockTime;
@@ -21,8 +20,11 @@ contract Lock {
     }
 
     function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        console.log(
+            'Unlock time is %o and block timestamp is %o',
+            unlockTime,
+            block.timestamp
+        );
 
         require(block.timestamp >= unlockTime, "You can't withdraw yet");
         require(msg.sender == owner, "You aren't the owner");
