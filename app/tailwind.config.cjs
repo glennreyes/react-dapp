2dfeac9501d286fdc7f15212efeff1aa48bd7ff8
Glenn Reyes <glenn@glennreyes.com>
2022-09-23 16:12:22 +0200

    feat: install inter web font

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const theme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   plugins: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...(theme.fontFamily.sans ?? [])],
+      },
+    },
   },
 };
