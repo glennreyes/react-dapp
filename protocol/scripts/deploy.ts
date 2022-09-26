373a5282f2b3ea17a1442086dcc5c1f9cd9eacea
Glenn Reyes <glenn@glennreyes.com>
2022-09-26 14:54:23 +0200

    feat: add greeter deploy tasks

--- protocol/scripts/deploy.ts
+++ protocol/scripts/deploy.ts
@@ -1,22 +1,31 @@
 import { ethers } from 'hardhat';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  /**
+   * LOCK
+   */
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const lockedAmount = ethers.utils.parseEther('1');
+  // const Lock = await ethers.getContractFactory('Lock');
+  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // await lock.deployed();
+  // console.log(
+  //   `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${
+  //     (lock as any).address
+  //   }`,
+  // );
 
-  const lockedAmount = ethers.utils.parseEther('1');
+  /**
+   * GREETER
+   */
+  const Greeter = await ethers.getContractFactory('Greeter');
+  const greeter = await Greeter.deploy('Hello, React Alicante!');
+  await greeter.deployed();
+  console.info(`Greeter deployed to ${(greeter as any).address}`);
 
-  const Lock = await ethers.getContractFactory('Lock');
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${
-      (lock as any).address
-    }`,
-  );
+  console.info('Deployment completed! 🚀');
 }
 
 // We recommend this pattern to be able to use async/await everywhere
